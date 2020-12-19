fc98e9892f0cad3b803ff99d883d5132b0df140e
Jasmine Sung <jasmine.t.sung@accenture.com>
2020-12-19 20:26:18 +0800

    clean up

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
@@ -9,7 +9,7 @@ class Convert(Resource):
         temp = float(request.args.get('temp'))
         if base == 'celsius':
             result = temp * 9/5 + 32
-        elif base == 'farenheit':
+        elif base == 'fahrenheit':
             result = (temp - 32) * 5/9
         print(result)
         return result
@@ -18,6 +18,3 @@ api.add_resource(Convert, '/convert')
 
 if __name__ == 'main':
     app.run()
-# @app.route('/')
-# def hello_world():
-#     return 'Hello world'
